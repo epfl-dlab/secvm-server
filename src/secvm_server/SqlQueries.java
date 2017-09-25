1c8db234109dedd02f71891cbbee5f413591d0db
valentin-hartmann-research <v.hartmann.research@gmail.com>
2017-09-25 22:59:27 +0200

    adapt test configuration fetching query to modified db design

--- src/secvm_server/SqlQueries.java
+++ src/secvm_server/SqlQueries.java
@@ -23,13 +23,16 @@ public enum SqlQueries {
 			"ORDER BY svm.svm_id, weight_vector.iteration DESC"),
 	
 	GET_TEST_CONFIGURATIONS(
-			"SELECT svm.svm_id, svm.num_bins, svm.dice_roll_probabilities, " + 
-			"	svm.test_outcomes_dice_roll, feature_vector.feature_type, " + 
-			"	feature_vector.num_features, feature_vector.num_hashes, weight_vector.weights\n" + 
-			"FROM svm, weight_vector, svm_uses_feature_vector feature_vector\n" + 
-			"WHERE svm.train_enabled = 1 AND svm.svm_id = weight_vector.svm_id " +
-			"	AND svm.svm_id = feature_vector.svm_id " +
-			"	AND (weight_vector.num_participants >= svm.min_number_train_participants OR weight_vector.iteration = 0)\n" + 
+			"SELECT svm.svm_id, dice_roll.probabilities, svm.test_outcomes_dice_roll" + 
+			"	feature_vector.feature_type, feature_vector.num_features," + 
+			"	feature_vector.num_hashes, weight_vector.weights, weight_vector.iteration\n" + 
+			"FROM svm, weight_vector, svm_uses_feature_vector, feature_vector, dice_roll\n" + 
+			"WHERE svm.test_enabled = 1 AND svm.svm_id = weight_vector.svm_id" + 
+			"	AND svm.svm_id = svm_uses_feature_vector.svm_id" + 
+			"	AND svm_uses_feature_vector.feature_vector_id = feature_vector.id" +
+			"	AND svm.dice_roll_id = dice_roll.id" +
+			"	AND (weight_vector.num_participants >= svm.min_number_train_participants" +
+			"		OR weight_vector.iteration = 0)\n" +
 			"ORDER BY svm.svm_id, weight_vector.iteration DESC");
 	
 	public final String query;
