47218f4195ff276181d4f4812d642d0867e7afa8
valentin-hartmann-research <v.hartmann.research@gmail.com>
2017-09-25 20:57:19 +0200

    add class to identify packages with train/test requests

---
+++ src/secvm_server/ServerRequestId.java
@@ -0,0 +1,40 @@
+package secvm_server;
+
+/**
+ * Indicates to which request from the server (i.e. to which svm)
+ * a train, test or participation package from a user belongs.
+ */
+public class ServerRequestId {
+	private int svmId;
+	private int iteration;
+	
+	public ServerRequestId(int svmId, int iteration) {
+		this.svmId = svmId;
+		this.iteration = iteration;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + iteration;
+		result = prime * result + svmId;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ServerRequestId other = (ServerRequestId) obj;
+		if (iteration != other.iteration)
+			return false;
+		if (svmId != other.svmId)
+			return false;
+		return true;
+	}
+}
