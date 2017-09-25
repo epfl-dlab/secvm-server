c1d9c8b0b39238ebc92bf5abbabb59282a0c46da
valentin-hartmann-research <v.hartmann.research@gmail.com>
2017-09-25 05:02:58 +0200

    add dummy returns

--- src/secvm_server/DataConverter.java
+++ src/secvm_server/DataConverter.java
@@ -1,15 +1,18 @@
 package secvm_server;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class DataConverter {
 	
 	public static <T extends Number> String numberListToBase64(List<T> list) {
-		
+		// TODO: implementation
+		return "";
 	}
 	
 	public static <T extends Number> List<T> base64ToNumberList(String string) {
-		
+		// TODO: implementation
+		return new ArrayList<>();
 	}
 
 }
