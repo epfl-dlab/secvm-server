71b9b3eccd4e3c9eb12a360ff2b507b6006477ec
valentin-hartmann-research <v.hartmann.research@gmail.com>
2017-09-25 04:02:40 +0200

    change variable names to camel case

--- src/secvm_server/TrainWeightsConfiguration.java
+++ src/secvm_server/TrainWeightsConfiguration.java
@@ -4,10 +4,10 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicReferenceArray;
 
 public class TrainWeightsConfiguration extends WeightsConfiguration {
-	private int min_number_train_participants;
-	private int num_participants;
+	private int minNumberTrainParticipants;
+	private int numParticipants;
 	private float lambda;
-	private List<Integer> train_outcomes_dice_roll;
+	private List<Integer> trainOutcomesDiceRoll;
 	
 	// the weight vector that is being sent to the users for training
 	private List<Float> weightsToUseForTraining;
@@ -20,31 +20,31 @@ public class TrainWeightsConfiguration extends WeightsConfiguration {
 	}
 	
 	public TrainWeightsConfiguration(int svmId, int iteration, int numBins, List<Float> diceRollProbabilities, List<FeatureVectorProperties> features,
-			int min_number_train_participants, int num_participants, float lambda, List<Integer> train_outcomes_dice_roll,
+			int minNumberTrainParticipants, int numParticipants, float lambda, List<Integer> trainOutcomesDiceRoll,
 			List<Float> weightsToUseForTraining, AtomicReferenceArray<Float> weightsBeingTrained) {
 		super(svmId, iteration, numBins, diceRollProbabilities, features);
-		this.min_number_train_participants = min_number_train_participants;
-		this.num_participants = num_participants;
+		this.minNumberTrainParticipants = minNumberTrainParticipants;
+		this.numParticipants = numParticipants;
 		this.lambda = lambda;
-		this.train_outcomes_dice_roll = train_outcomes_dice_roll;
+		this.trainOutcomesDiceRoll = trainOutcomesDiceRoll;
 		this.weightsToUseForTraining = weightsToUseForTraining;
 		this.weightsBeingTrained = weightsBeingTrained;
 	}
 	
-	public int getMin_number_train_participants() {
-		return min_number_train_participants;
+	public int getMinNumberTrainParticipants() {
+		return minNumberTrainParticipants;
 	}
 
-	public void setMin_number_train_participants(int min_number_train_participants) {
-		this.min_number_train_participants = min_number_train_participants;
+	public void setMinNumberTrainParticipants(int minNumberTrainParticipants) {
+		this.minNumberTrainParticipants = minNumberTrainParticipants;
 	}
 
-	public int getNum_participants() {
-		return num_participants;
+	public int getNumParticipants() {
+		return numParticipants;
 	}
 
-	public void setNum_participants(int num_participants) {
-		this.num_participants = num_participants;
+	public void setNumParticipants(int numParticipants) {
+		this.numParticipants = numParticipants;
 	}
 
 	public float getLambda() {
@@ -55,12 +55,12 @@ public class TrainWeightsConfiguration extends WeightsConfiguration {
 		this.lambda = lambda;
 	}
 
-	public List<Integer> getTrain_outcomes_dice_roll() {
-		return train_outcomes_dice_roll;
+	public List<Integer> getTrainOutcomesDiceRoll() {
+		return trainOutcomesDiceRoll;
 	}
 
-	public void setTrain_outcomes_dice_roll(List<Integer> train_outcomes_dice_roll) {
-		this.train_outcomes_dice_roll = train_outcomes_dice_roll;
+	public void setTrainOutcomesDiceRoll(List<Integer> trainOutcomesDiceRoll) {
+		this.trainOutcomesDiceRoll = trainOutcomesDiceRoll;
 	}
 
 	public List<Float> getWeightsToUseForTraining() {
