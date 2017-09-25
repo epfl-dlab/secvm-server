89feb60f2a99a159b08e78eee6dac1d3a242cdbe
valentin-hartmann-research <v.hartmann.research@gmail.com>
2017-09-26 01:50:59 +0200

    add vector arithmetic (summation, division)

--- src/secvm_server/DataUtils.java
+++ src/secvm_server/DataUtils.java
@@ -14,5 +14,17 @@ public final class DataUtils {
 		// TODO: implementation
 		return new ArrayList<>();
 	}
+	
+	public static void addToFirstVector(List<Float> l1, List<Float> l2) {
+		for (int i = 0; i < l1.size(); ++i) {
+			l1.set(i, l1.get(i) + l2.get(i));
+		}
+	}
+	
+	public static void divideVector(List<Float> list, float divisor) {
+		for (int i = 0; i < list.size(); ++i) {
+			list.set(i, list.get(i) / divisor);
+		}
+	}
 
 }
