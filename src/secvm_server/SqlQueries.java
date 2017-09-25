89b613ec6b953ae3dce4cbbd9ca42bb4c09b406e
valentin-hartmann-research <v.hartmann.research@gmail.com>
2017-09-26 01:49:13 +0200

    fix bug in test configurations fetching query

--- src/secvm_server/SqlQueries.java
+++ src/secvm_server/SqlQueries.java
@@ -23,12 +23,12 @@ public enum SqlQueries {
 			"ORDER BY svm.svm_id, weight_vector.iteration DESC"),
 	
 	GET_TEST_CONFIGURATIONS(
-			"SELECT svm.svm_id, dice_roll.probabilities, svm.test_outcomes_dice_roll" + 
+			"SELECT svm.svm_id, dice_roll.probabilities, svm.test_outcomes_dice_roll," + 
 			"	feature_vector.feature_type, feature_vector.num_features," + 
 			"	feature_vector.num_hashes, weight_vector.weights, weight_vector.iteration\n" + 
 			"FROM svm, weight_vector, svm_uses_feature_vector, feature_vector, dice_roll\n" + 
-			"WHERE svm.test_enabled = 1 AND svm.svm_id = weight_vector.svm_id" + 
-			"	AND svm.svm_id = svm_uses_feature_vector.svm_id" + 
+			"WHERE svm.test_enabled = 1 AND svm.svm_id = weight_vector.svm_id" +
+			"	AND svm.svm_id = svm_uses_feature_vector.svm_id" +
 			"	AND svm_uses_feature_vector.feature_vector_id = feature_vector.id" +
 			"	AND svm.dice_roll_id = dice_roll.id" +
 			"	AND (weight_vector.num_participants >= svm.min_number_train_participants" +
