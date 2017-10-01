94f60e0f41e6dbd791bbe21b3f63ab112eec0b98
valentin-hartmann-research <v.hartmann.research@gmail.com>
2017-10-01 22:34:16 +0200

    add functions to apply the subgradient update

--- src/secvm_server/DataUtils.java
+++ src/secvm_server/DataUtils.java
@@ -2,8 +2,26 @@ package secvm_server;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public final class DataUtils {
+	
+	public static float computeStepSize(int iteration, float lambda) {
+		return lambda / iteration;
+	}
+	
+	public static void applySubgradientUpdate(
+			List<Float> weights, int iteration, float lambda,
+			AtomicIntegerArray subgradient, int numGradientUpdateVectors) {
+		
+		float stepSize = computeStepSize(iteration, lambda);
+		
+		for (int i = 0; i < weights.size(); ++i) {
+			weights.set(i,
+					(1 - stepSize * lambda) * weights.get(i) +
+					subgradient.get(i) / (float) numGradientUpdateVectors);
+		}
+	}
 	
 	public static <T extends Number> String numberListToBase64(List<T> list) {
 		// TODO: implementation
@@ -13,6 +31,16 @@ public final class DataUtils {
 	public static <T extends Number> List<T> base64ToNumberList(String string) {
 		// TODO: implementation
 		return new ArrayList<>();
+	}
+	
+	public static String atomicIntegerArrayToBase64(AtomicIntegerArray array) {
+		// TODO: implementation
+		return "";
+	}
+	
+	public static AtomicIntegerArray base64ToAtomicIntegerArray(String string) {
+		// TODO: implementation
+		return new AtomicIntegerArray(0);
 	}
 	
 	public static void addToFirstVector(List<Float> l1, List<Float> l2) {
